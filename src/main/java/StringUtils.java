8f373ca783f9f64b8d3d472da518d8f2c06549ad
nagatax <4foview@gmail.com>
2019-05-19 14:16:13 +0900

    pomファイルの編集
    CheckstyleとFindbugs、JaCoCoのpluginを追加する。

--- src/main/java/StringUtils.java
+++ src/main/java/StringUtils.java
@@ -4,12 +4,17 @@ import java.util.regex.Pattern;
 public class StringUtils {
 
     public static String toSnakeCase(String text) {
-        if (text == null) throw new NullPointerException("text == null.");
+        if (text == null) {
+            throw new NullPointerException("text == null.");
+        }
+
         String snake = text;
         Pattern p = Pattern.compile("([A-Z])");
         for (; ; ) {
             Matcher m = p.matcher(snake);
-            if (!m.find()) break;
+            if (!m.find()) {
+                break;
+            }
             snake = m.replaceFirst("_" + m.group(1).toLowerCase());
         }
         //
